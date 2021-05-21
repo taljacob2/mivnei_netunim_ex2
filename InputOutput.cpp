8884a25f30b00c4ee80ada0a030dcaf1f2064d0c
TalJacob <taljacob@gmail.com>
2021-05-21 11:58:55 +0300

    good organization of InputOutput

--- InputOutput.cpp
+++ InputOutput.cpp
@@ -1,8 +1,7 @@
-//
-// Created by Tal on 26-Mar-21.
-//
 
 #include "InputOutput.h"
+
+/* -- Constructor & Destructor -- */
 
 InputOutput::InputOutput(int &N, int &K, std::string &inputFileName,
                          std::string &outputFileName)
@@ -16,22 +15,120 @@ InputOutput::InputOutput(int &N, int &K, std::string &inputFileName,
 
 InputOutput::~InputOutput() = default;
 
-int InputOutput::getCheckedUnsignedInt(std::istream &is) noexcept(false) {
-    return getChecked<int>(is, InputOutput::checkUnsignedIntInput, atoi);
+/* -- Getters -- */
+
+int InputOutput::getN() const { return N; }
+
+int InputOutput::getK() const { return K; }
+
+const std::string &InputOutput::getInputFileName() const {
+    return INPUT_FILE_NAME;
 }
+
+const std::string &InputOutput::getOutputFileName() const {
+    return OUTPUT_FILE_NAME;
+}
+
+/* -- Main Methods -- */
+
+void InputOutput::receiveInput(int &N, int &K, std::string &inputFileName,
+                               std::string &outputFileName) {
+
+    /* Receive N */
+    N = getCheckedInt(std::cin);
+
+    /* Receive K */
+    K = getCheckedUnsignedInt(std::cin);
+
+    /* Receive name of InputFile: */
+    std::cin >> inputFileName; // TODO: need to check this somehow?
+
+    /* Receive name of OutputFile: */
+    std::cin >> outputFileName; // TODO: need to check this somehow?
+}
+
+InputOutput *InputOutput::factoryInputOutput() {
+
+    /* Receive input: */
+    int         N;
+    int         K;
+    std::string INPUT_FILE_NAME;
+    std::string OUTPUT_FILE_NAME;
+    InputOutput::receiveInput(N, K, INPUT_FILE_NAME, OUTPUT_FILE_NAME);
+
+    /* Create InputOutput object */
+    return new InputOutput(N, K, INPUT_FILE_NAME, OUTPUT_FILE_NAME);
+}
+
+int *InputOutput::getIntArrayFromInputFile() {
+
+    // TODO: maybe split to smaller methods.
+
+    std::ifstream file(INPUT_FILE_NAME);
+    if (!file) {
+
+        /* Could not find the file. Throw a message. */
+        std::string message;
+        message.append("Could not find the file: ");
+        message.append(INPUT_FILE_NAME);
+        message.append("\n");
+
+        // TODO: change to `wrong input`
+        throw std::runtime_error(message);
+    }
+
+    auto *array = new int[N];
+    int   i     = 0;
+    while ((i < N) && (!file.eof())) {
+        try {
+            array[i] = getCheckedInt(file);
+        } catch (std::exception &e) {
+            delete[] array;
+            throw;
+        }
+        i++;
+    }
+    if (i < N) {
+
+        /*
+         * Means, the file ended earlier than indicated in the provided
+         * `N` parameter. This is an error. Throw a message.
+         */
+        std::string message;
+        message.append("We have reached the end of the file earlier than "
+                       "expected.\nThe provided `N` parameter is "
+                       "larger than the number of elements received.");
+        message.append("\n");
+        message.append("The `N` provided was: ").append(std::to_string(N));
+        message.append(", and the number of elements received is: ")
+                .append(std::to_string(i))
+                .append(".");
+        message.append("\n");
+
+        // TODO: change to `wrong input`
+        delete[] array;
+        throw std::runtime_error(message);
+    }
+
+    file.close();
+    return array;
+}
+
+/* -- Get Checkers -- */
 
 int InputOutput::getCheckedInt(std::istream &is) noexcept(false) {
     return getChecked<int>(is, InputOutput::checkIntInput, atoi);
 }
 
-bool InputOutput::checkUnsignedIntInput(std::string &input) {
-    for (char c : input) {
-        if (c > '9' || c < '0') {
-            return false; // input is invalid.
-        }
-    }
-    return true; // input is valid.
+int InputOutput::getCheckedUnsignedInt(std::istream &is) noexcept(false) {
+    return getChecked<int>(is, InputOutput::checkUnsignedIntInput, atoi);
 }
+
+int InputOutput::getCheckedPositiveInt(std::istream &is) noexcept(false) {
+    return getChecked<int>(is, InputOutput::checkPositiveIntInput, atoi);
+}
+
+/* -- Checkers -- */
 
 bool InputOutput::checkIntInput(std::string &input) {
     bool returnValue = false;
@@ -53,20 +150,71 @@ bool InputOutput::checkIntInput(std::string &input) {
             break;
         }
     }
+
+    /* Return `true` only if all iterations have passed successfully. */
     if (i == input.length()) { returnValue = true; }
     return returnValue;
 }
 
-/* -- Getters & Setters -- */
+bool InputOutput::checkUnsignedIntInput(std::string &input) {
+    bool returnValue = false;
+    int  i           = 0;
+    for (; i < input.length(); i++) {
+        if (input[i] == '+') {
+            if (i == 0) {
 
-int InputOutput::getN() const { return N; }
+                /* Okay, only if this is the `first` char in the string. */
+                continue;
+            } else {
 
-int InputOutput::getK() const { return K; }
+                /* This is not the `first` char in the string. */
+                break;
+            }
+        } else if (!(('0' <= input[i]) && (input[i] <= '9'))) {
 
-const std::string &InputOutput::getInputFileName() const {
-    return INPUT_FILE_NAME;
+            /* If this is not a number. */
+            break;
+        }
+    }
+
+    /* Return `true` only if all iterations have passed successfully. */
+    if (i == input.length()) { returnValue = true; }
+    return returnValue;
 }
 
-const std::string &InputOutput::getOutputFileName() const {
-    return OUTPUT_FILE_NAME;
+bool InputOutput::checkPositiveIntInput(std::string &input) {
+    bool returnValue = false;
+    int  i           = 0;
+    for (; i < input.length(); i++) {
+        if (input[i] == '+') {
+            if (i == 0) {
+
+                /* Okay, only if this is the `first` char in the string. */
+                continue;
+            } else {
+
+                /* This is not the `first` char in the string. */
+                break;
+            }
+        }
+        if (input[i] == '0') {
+            if (i == 0) {
+
+                /* Bad, only if this is the `first` char in the string. */
+                break;
+            } else {
+
+                /* This is not the `first` char in the string. */
+                continue;
+            }
+        } else if (!(('0' <= input[i]) && (input[i] <= '9'))) {
+
+            /* If this is not a number. */
+            break;
+        }
+    }
+
+    /* Return `true` only if all iterations have passed successfully. */
+    if (i == input.length()) { returnValue = true; }
+    return returnValue;
 }
--- InputOutput.h
+++ InputOutput.h
@@ -34,188 +34,24 @@ class InputOutput {
     /// The name of the 'Measure' file:
     static constexpr char *fileName = (char *) "Measure.txt";
 
+    /* -- Constructor & Destructor -- */
   public:
     InputOutput(int &N, int &K, std::string &inputFileName,
                 std::string &outputFileName);
 
     ~InputOutput();
 
-    /* -- Getters & Setters -- */
-
+    /* -- Getters -- */
+  public:
     int                getN() const;
     int                getK() const;
     const std::string &getInputFileName() const;
     const std::string &getOutputFileName() const;
 
-    /**
-     * this *static* method measures the time it takes
-     * to a given @p functionToMeasure
-     * to work on the @p array given.
-     * the method outputs the measures to the
-     * `this::fileName` `Measure` file.
-     *
-     * @tparam T element of the array.
-     * @param array array to be manipulated.
-     * @param size size of the given array.
-     * @param functionToMeasure the method that manipulates the array.
-     * @return the `new` allocated manipulated array.
-     * @see my_algorithms
-     * @see `this::fileName`
-     */
-    template<class T>
-    static T *printMeasure(
-            T *array, int size,
-            const std::function<void(T *, int, char *&)> &functionToMeasure) {
-
-        // copy the array to another array:
-        T *arrayToManipulate = my_algorithms::copyArray(array, size);
-
-        // getChecked the function name:
-        char *nameOfFunctionToMeasure;
-
-        auto start = std::chrono::high_resolution_clock::now();
-
-        // unsync the I/O of C and C++.
-        std::ios_base::sync_with_stdio(false);
-
-        // Here you put the name of the function you wish to measure:
-        functionToMeasure(arrayToManipulate, size, nameOfFunctionToMeasure);
-        auto end = std::chrono::high_resolution_clock::now();
-
-        // Calculating total time taken by the program.
-        calculateTotalTimeTakenByTheProgram(start, end,
-                                            nameOfFunctionToMeasure);
-
-        // return the manipulated array:
-        return arrayToManipulate;
-    }
-
-    /**
-    * this *static* method measures the time it takes
-    * to a given @p functionToMeasure
-    * to work on the @p array given.
-    * the method outputs the measures to the
-    * `this::fileName` `Measure` file.
-    *
-    * @tparam T element of the array.
-    * @param array array to be manipulated.
-    * @param size size of the given array.
-    * @param functionToMeasure the method that manipulates the array.
-    * @return the `new` allocated manipulated array.
-    * @see my_algorithms
-    * @see `this::fileName`
-    */
-    template<class T>
-    static T printMeasure(
-            T *array, int size, int serialSizeOfTheElementToLookFor,
-            const std::function<T(T *, int, int, char *&)> &functionToMeasure) {
-
-        // copy the array to another array:
-        T *arrayToManipulate = my_algorithms::copyArray(array, size);
-
-        // getChecked the function name:
-        char *nameOfFunctionToMeasure;
-
-        auto start = std::chrono::high_resolution_clock::now();
-
-        // unsync the I/O of C and C++.
-        std::ios_base::sync_with_stdio(false);
-
-        // Here you put the name of the function you wish to measure:
-        auto elementRequested = functionToMeasure(
-                arrayToManipulate, size, serialSizeOfTheElementToLookFor,
-                nameOfFunctionToMeasure);
-        auto end = std::chrono::high_resolution_clock::now();
-
-        // Calculating total time taken by the program.
-        calculateTotalTimeTakenByTheProgram(start, end,
-                                            nameOfFunctionToMeasure);
-
-        // delete copied array:
-        delete[] arrayToManipulate;
-
-        // return the manipulated array:
-        return elementRequested;
-    }
-
-    template<class T>
-    static void setSorts(T *array, int size,
-                         int serialSizeOfTheElementToLookFor,
-                         T *&insertionSortedArray, T &selectionElementRequested,
-                         T &quintupletElementRequested) {
-
-        // insertion-sort:
-        insertionSortedArray = InputOutput::printMeasure<double>(
-                array, size, my_algorithms::sort_insertion_<double>);
-
-        // selection:
-        selectionElementRequested = InputOutput::printMeasure<double>(
-                array, size, serialSizeOfTheElementToLookFor,
-                my_algorithms::selection_TheIthElementInItsSize_<double>);
-
-        // quintuplet:
-        quintupletElementRequested = InputOutput::printMeasure<double>(
-                array, size, serialSizeOfTheElementToLookFor,
-                my_algorithms::quintuplet_TheIthElementInItsSize_<double>);
-    }
-
-    template<class T>
-    static void printElements(T * insertionSortedArray,
-                              T & selectionElementRequested,
-                              T & quintupletElementRequested,
-                              int serialSizeOfTheElementToLookFor) {
-
-        // insertion:
-        std::cout << "Insertion sort i'th element: ";
-        std::cout << insertionSortedArray[serialSizeOfTheElementToLookFor - 1]
-                  << std::endl;
-
-        // selection:
-        std::cout << "Selection i'th element: ";
-        std::cout << selectionElementRequested << std::endl;
-
-        // quintuplet:
-        std::cout << "Quintuplet algorithm i'th element: ";
-        std::cout << quintupletElementRequested << std::endl;
-    }
-
-  private:
-    static void calculateTotalTimeTakenByTheProgram(
-            std::chrono::time_point<std::chrono::high_resolution_clock> start,
-            std::chrono::time_point<std::chrono::high_resolution_clock> end,
-            const char *nameOfFunctionToMeasure) {
-
-        // Calculating total time taken by the program.
-        double time_taken =
-                std::chrono::duration_cast<std::chrono::nanoseconds>(end -
-                                                                     start)
-                        .count();
-        time_taken *= 1e-9;
-
-        // The name of the file, append writing:
-        std::ofstream file(fileName, std::ios_base::app);
-        file << "Time taken by function " << nameOfFunctionToMeasure
-             << " is : " << std::setprecision(9) << std::fixed << time_taken;
-        file << " sec" << std::endl;
-        file.close();
-    }
-
+    /* -- Main Methods -- */
   public:
     static void receiveInput(int &N, int &K, std::string &inputFileName,
-                             std::string &outputFileName) {
-
-        /* Receive N */
-        N = getCheckedInt(std::cin);
-
-        /* Receive K */
-        K = getCheckedUnsignedInt(std::cin);
-
-        /* Receive name of InputFile: */
-        std::cin >> inputFileName; // TODO: need to check this somehow?
-
-        /* Receive name of OutputFile: */
-        std::cin >> outputFileName; // TODO: need to check this somehow?
-    }
+                             std::string &outputFileName);
 
     /**
      * @brief This method serves as a **Factory** method.
@@ -233,18 +69,7 @@ class InputOutput {
      * and returns it.
      * @see InputOutput(int&, int&, std::string&, std::string&)
      */
-    static InputOutput *factoryInputOutput() {
-
-        /* Receive input: */
-        int         N;
-        int         K;
-        std::string INPUT_FILE_NAME;
-        std::string OUTPUT_FILE_NAME;
-        InputOutput::receiveInput(N, K, INPUT_FILE_NAME, OUTPUT_FILE_NAME);
-
-        /* Create InputOutput object */
-        return new InputOutput(N, K, INPUT_FILE_NAME, OUTPUT_FILE_NAME);
-    }
+    static InputOutput *factoryInputOutput();
 
     /**
      * @brief This method gets the `array` specified in the *INPUT_FILE*
@@ -257,96 +82,17 @@ class InputOutput {
      *                            is larger than the actual number of elements
      *                            provided in the *file*.
      */
-    int *getIntArrayFromInputFile() {
+    int *getIntArrayFromInputFile();
 
-        // TODO: maybe split to smaller methods.
-
-        std::ifstream file(INPUT_FILE_NAME);
-        if (!file) {
-
-            /* Could not find the file. Throw a message. */
-            std::string message;
-            message.append("Could not find the file: ");
-            message.append(INPUT_FILE_NAME);
-            message.append("\n");
-
-            // TODO: change to `wrong input`
-            throw std::runtime_error(message);
-        }
-
-        auto *array = new int[N];
-        int   i     = 0;
-        while ((i < N) && (!file.eof())) {
-            try {
-                array[i] = getCheckedInt(file);
-            } catch (std::exception &e) {
-                delete[] array;
-                throw;
-            }
-            i++;
-        }
-        if (i < N) {
-
-            /*
-             * Means, the file ended earlier than indicated in the provided
-             * `N` parameter. This is an error. Throw a message.
-             */
-            std::string message;
-            message.append("We have reached the end of the file earlier than "
-                           "expected.\nThe provided `N` parameter is "
-                           "larger than the number of elements received.");
-            message.append("\n");
-            message.append("The `N` provided was: ").append(std::to_string(N));
-            message.append(", and the number of elements received is: ")
-                    .append(std::to_string(i))
-                    .append(".");
-            message.append("\n");
-
-            // TODO: change to `wrong input`
-            delete[] array;
-            throw std::runtime_error(message);
-        }
-
-        file.close();
-        return array;
-    }
-
+    /* -- Get Checkers -- */
+  public:
     static int getCheckedInt(std::istream &is);
 
     static int getCheckedUnsignedInt(std::istream &is);
 
+    static int getCheckedPositiveInt(std::istream &is);
+
   private:
-    /**
-     * @brief This method receives as a parameter a `std::string` and checks
-     *        whether it is a legal **UnsignedInt**.
-     *
-     * This method is *private*, and is being used as a parameter for the
-     * invocation of @link getCheckedUnsignedInt(std::string &) @endlink in
-     * the body of @link getChecked @endlink method.
-     * @param input the `input` gotten, that is needed to be checked.
-     * @return @li `true` if the `input` is `legal / valid`.
-     *         @li `false` if the `input` is `illegal / invalid`.
-     * @see getCheckedUnsignedInt(std::istream &)
-     * @see getChecked
-     */
-    static bool checkUnsignedIntInput(std::string &input);
-
-    /**
-     * @brief This method receives as a parameter a `std::string` and checks
-     *        whether it is a legal **Int**.
-     *
-     * This method is *private*, and is being used as a parameter for the
-     * invocation of @link getCheckedInt(std::string &) @endlink in
-     * the body of @link getChecked @endlink method.
-     * @param input the `input` gotten, that is needed to be checked.
-     * @return @li `true` if the `input` is `legal / valid`.
-     *         @li `false` if the `input` is `illegal / invalid`.
-     * @see getCheckedInt(std::istream &)
-     * @see getChecked
-     */
-    static bool checkIntInput(std::string &input);
-
-  public:
     /**
      * @brief This method gets an input from an `input-stream` @p is,
      *        and after that, *checks* whether the input gotten is legal by
@@ -372,7 +118,7 @@ class InputOutput {
                const std::function<bool(std::string &)> &checkingFunction,
                const std::function<T(const char *)> &    returnFunction) {
 
-        // getChecked the input
+        // `getChecked` the input.
         std::string stringInput;
         std::getline(is, stringInput);
         if (checkingFunction(stringInput)) {
@@ -384,6 +130,53 @@ class InputOutput {
             throw std::runtime_error(msg);
         }
     }
+
+    /* -- Checkers -- */
+  private:
+    /**
+     * @brief This method receives as a parameter a `std::string` and checks
+     *        whether it is a legal **Int**.
+     *
+     * This method is *private*, and is being used as a parameter for the
+     * invocation of @link getCheckedInt(std::string &) @endlink in
+     * the body of @link getChecked @endlink method.
+     * @param input the `input` gotten, that is needed to be checked.
+     * @return @li `true` if the `input` is `legal / valid`.
+     *         @li `false` if the `input` is `illegal / invalid`.
+     * @see getCheckedInt(std::istream &)
+     * @see getChecked
+     */
+    static bool checkIntInput(std::string &input);
+
+    /**
+     * @brief This method receives as a parameter a `std::string` and checks
+     *        whether it is a legal **UnsignedInt**.
+     *
+     * This method is *private*, and is being used as a parameter for the
+     * invocation of @link getCheckedUnsignedInt(std::string &) @endlink in
+     * the body of @link getChecked @endlink method.
+     * @param input the `input` gotten, that is needed to be checked.
+     * @return @li `true` if the `input` is `legal / valid`.
+     *         @li `false` if the `input` is `illegal / invalid`.
+     * @see getCheckedUnsignedInt(std::istream &)
+     * @see getChecked
+     */
+    static bool checkUnsignedIntInput(std::string &input);
+
+    /**
+ * @brief This method receives as a parameter a `std::string` and checks
+ *        whether it is a legal **UnsignedInt**.
+ *
+ * This method is *private*, and is being used as a parameter for the
+ * invocation of @link getCheckedUnsignedInt(std::string &) @endlink in
+ * the body of @link getChecked @endlink method.
+ * @param input the `input` gotten, that is needed to be checked.
+ * @return @li `true` if the `input` is `legal / valid`.
+ *         @li `false` if the `input` is `illegal / invalid`.
+ * @see getCheckedUnsignedInt(std::istream &)
+ * @see getChecked
+ */
+    static bool checkPositiveIntInput(std::string &input);
 };
 
 #endif //MIVNEI_NETUNIM_EX2_INPUTOUTPUT_H
