d60b106345e7e29139f5626ab4007e7b098f6547
TalJacob <taljacob@gmail.com>
2021-05-22 21:43:09 +0300

    works perfectly. need now to export to outputFile2.

--- doc/html/search/functions_3.js
+++ doc/html/search/functions_3.js
@@ -1,4 +1,4 @@
-var searchData =
-    [
-        ['entry_113', ['Entry', ['../class_entry.html#a82da84e98e788f321b0e5e969ebe7501', 1, 'Entry::Entry(K key, V value)'], ['../class_entry.html#ac799c4ef2762137b37348d7af6adfcc8', 1, 'Entry::Entry()=default']]]
-    ];
+var searchData=
+[
+  ['entry_113',['Entry',['../class_entry.html#a82da84e98e788f321b0e5e969ebe7501',1,'Entry::Entry(K key, V value)'],['../class_entry.html#ac799c4ef2762137b37348d7af6adfcc8',1,'Entry::Entry()=default']]]
+];
