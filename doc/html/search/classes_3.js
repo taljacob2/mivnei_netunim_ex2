fbf69399594d8ce2413af094e34f4743902ec99a
TalJacob <taljacob@gmail.com>
2021-05-22 16:28:19 +0300

    works with memory-leaks - after forcing value to be int2

---
+++ doc/html/search/classes_3.js
@@ -0,0 +1,6 @@
+var searchData =
+    [
+        ['minheap_90', ['MinHeap', ['../class_min_heap.html', 1, '']]],
+        ['minheapadt_91', ['MinHeapADT', ['../class_min_heap_a_d_t.html', 1, '']]],
+        ['my_5falgorithms_92', ['my_algorithms', ['../classmy__algorithms.html', 1, '']]]
+    ];
