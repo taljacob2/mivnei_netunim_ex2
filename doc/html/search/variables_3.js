73da2cb43fa049b45f4366c04cd24359141daa4d
TalJacob <taljacob@gmail.com>
2021-05-22 21:39:41 +0300

    works perfectly. need now to export to outputFile2.

--- doc/html/search/variables_3.js
+++ doc/html/search/variables_3.js
@@ -1,4 +1,4 @@
-var searchData =
-    [
-        ['logicalsize_171', ['logicalSize', ['../class_min_heap.html#aa5f4e762e12a9ea582b476c9520f2e91', 1, 'MinHeap']]]
-    ];
+var searchData=
+[
+  ['logicalsize_171',['logicalSize',['../class_min_heap.html#aa5f4e762e12a9ea582b476c9520f2e91',1,'MinHeap']]]
+];
