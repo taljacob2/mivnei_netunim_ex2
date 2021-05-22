47fb86de4e8a0bbaab2fb35f28e55f54a120b14b
TalJacob <taljacob@gmail.com>
2021-05-22 21:39:41 +0300

    works perfectly. need now to export to outputFile2.

--- doc/html/search/variables_0.js
+++ doc/html/search/variables_0.js
@@ -1,4 +1,4 @@
-var searchData =
-    [
-        ['array_167', ['array', ['../class_min_heap.html#a889e4bc48d8430fa841532b25dc000e4', 1, 'MinHeap']]]
-    ];
+var searchData=
+[
+  ['array_167',['array',['../class_min_heap.html#a889e4bc48d8430fa841532b25dc000e4',1,'MinHeap']]]
+];
--- doc/html/search/variables_7.js
+++ doc/html/search/variables_7.js
@@ -1,4 +1,6 @@
-var searchData =
-    [
-        ['value_175', ['value', ['../class_entry.html#afb034c5a062161937891750ef054c63b', 1, 'Entry']]]
-    ];
+declare
+decorator @wrap(wrapper: (obj: any) => any)
+;
+declare
+decorator @register(callback: (target, propertyKey?)
+=
