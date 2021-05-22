9ef995cf104960628699ed3fe2089856b01e235d
TalJacob <taljacob@gmail.com>
2021-05-22 21:40:45 +0300

    works perfectly. need now to export to outputFile2.

---
+++ cmake-build-debug/CMakeFiles/mivnei_netunim_ex2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mivnei_netunim_ex2.dir/InputOutput.cpp.o"
+  "CMakeFiles/mivnei_netunim_ex2.dir/main.cpp.o"
+  "mivnei_netunim_ex2"
+  "mivnei_netunim_ex2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mivnei_netunim_ex2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
